66275652ae969d4801b1c49076548411248e589c
IvanHalych <halych0799@gmail.com>
2018-05-20 12:09:34 +0300

    Dal

---
+++ CVAndVacancyBase.DAL/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CVAndVacancyBase.DLL.Entities.Abstract;
+
+namespace CVAndVacancyBase.DLL.Entities
+{
+    public class User : Entity
+    {
+        private string email;
+        private string password;
+        private string name;
+        private bool admin;
+
+        public string Email { get => email; set => email = value; }
+        public string Password { get => password; set => password = value; }
+        public string Name { get => name; set => name = value; }
+        public bool Admin { get => admin; set => admin = value; }
+    }
+}
+
---
+++ CVAndVacancyBase.DAL/Repositories/VacancyRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CVAndVacancyBase.DLL.Repositories.Abstract;
+using CVAndVacancyBase.DLL.EF;
+using CVAndVacancyBase.DLL.Entities;
+using System.Data.Entity;
+
+namespace CVAndVacancyBase.DLL.Repositories
+{
+    class VacancyRepository:IRepositiry<Vacancy>
+    {
+        private ModelContext db;
+
+        public VacancyRepository(ModelContext db)
+        {
+            this.db = db;
+        }
+        public IEnumerable<Vacancy> GetList()
+        {
+            return db.Vacancies;
+        }
+        public Vacancy Get(int id)
+        {
+            return db.Vacancies.Find(id);
+        }
+        public void Create(Vacancy vac)
+        {
+            db.Vacancies.Add(vac);
+        }
+        public void Update(Vacancy vac)
+        {
+            db.Entry(vac).State = EntityState.Modified;
+        }
+        public void Delete(int id)
+        {
+            Vacancy vac = db.Vacancies.Find(id);
+            if (vac != null)
+                db.Vacancies.Remove(vac);
+        }
+    }
+}
